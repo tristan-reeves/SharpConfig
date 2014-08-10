ee761a4de1236f128014d34d51ccf0e8f399135f
Tristan <treeves@gmail.com>
2014-08-10 22:48:53 +1000

    make default install work from nuget

--- Common/VersionInfo.cs
+++ Common/VersionInfo.cs
@@ -22,5 +22,5 @@
 // 
 // 
 using System.Reflection; 
-[assembly: AssemblyVersion("2.0.0.8")] 
-[assembly: AssemblyFileVersion("2.0.0.8")] 
+[assembly: AssemblyVersion("1.0.0.0")] 
+[assembly: AssemblyFileVersion("1.0.0.0")] 
