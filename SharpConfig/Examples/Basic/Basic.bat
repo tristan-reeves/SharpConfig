d8749bce8e578e7ea47f2ebb5121b436cb74ce3c
Tristan <treeves@gmail.com>
2014-08-10 11:49:38 +1000

    adjust nuget package

--- SharpConfig/Examples/Basic/Basic.bat
+++ SharpConfig/Examples/Basic/Basic.bat
@@ -1,4 +1,4 @@
 @echo off
 
-%~dp0..\..\bin\SharpConfig.exe --config-source Source\Values.csv --output-directory ConfigOutput --base-directory %~dp0
+"%~dp0..\..\bin\SharpConfig.exe" --config-source ..\ConfigSource\ConfigValues.csv --output-directory ConfigOutput --base-directory "%~dp0\ "
 
