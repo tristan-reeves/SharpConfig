226eb6d56dc4f047b9cf7b48d32b5d51cb55251d
Tristan <treeves@gmail.com>
2014-08-10 22:26:30 +1000

    much work on nuget installers

--- Common/VersionInfo.cs
+++ Common/VersionInfo.cs
@@ -22,5 +22,5 @@
 // 
 // 
 using System.Reflection; 
-[assembly: AssemblyVersion("1.0.0.4")] 
-[assembly: AssemblyFileVersion("1.0.0.4")] 
+[assembly: AssemblyVersion("2.0.0.8")] 
+[assembly: AssemblyFileVersion("2.0.0.8")] 
